655dfdd607d95523fb18b5c29327b730f9bf051c
Li-aung 'Lewis' Yip <liaung@gmail.com>
2016-07-10 03:07:49 +0800

    More refactoring

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ from html2soup import break_into_sections
 from process_section import ProcessSection
 from write_markdown import print_to_markdown
 
-f = open(
-    "C:\Users\lws\gits\pcgen\docs\listfilepages\globalfilestagpages\globalfilesbonus.html",
-    mode="r")
-contents = f.read()
 
-bigsoup = bs4.BeautifulSoup(contents)
+def process_file(input_file_path):
+    with open(input_file_path, mode="r") as in_file:
+        whole_file_html = in_file.read()
 
-# Remove all spurious "\n" strings, which are inserted whenever the HTML had
-# whitespace between tags
-for e in bigsoup.find_all(text=u"\n"):
-    e.extract()
+    whole_file_soup = bs4.BeautifulSoup(whole_file_html)
 
-body = bigsoup.body
+    # Remove all spurious "\n" strings, which are inserted whenever the HTML had
+    # whitespace between tags
+    for element in whole_file_soup.find_all(text=u"\n"):
+        element.extract()
 
-sections = break_into_sections(body)
+    body = whole_file_soup.body # HTML <body>
+    soup_sections = break_into_sections(body) # Break up by <hr /> tags
 
-for s in sections:
-    print_to_markdown(ProcessSection(s))
+    for soup_section in soup_sections:
+        processed_section = ProcessSection(soup_section)
+        print_to_markdown(processed_section)
+
+
+ex1 = "C:\Users\lws\gits\pcgen\docs\listfilepages\globalfilestagpages\globalfilesbonus.html"
+process_file(ex1)
