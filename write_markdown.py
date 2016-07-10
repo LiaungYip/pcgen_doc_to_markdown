1ee4a6c6b2737a035c1ef004448721d38431fdba
Li-aung 'Lewis' Yip <liaung@gmail.com>
2016-07-10 20:36:36 +0800

    Give each markdown file a (hopefully) unique ID so the menus all work.

--- write_markdown.py
+++ write_markdown.py
@@ -7,6 +7,7 @@ title = "{anchor}"
 original_url = "{original_url}"
 
 [menu.main]
+    identifier = "{id}"
     parent = "{parent}"
 +++
 # {anchor}
@@ -28,24 +29,27 @@ original_url = "{original_url}"
 
 
 def generate_file_name(output_dir, anchor_name):
-    def assemble_name(seq=None):
+    def seq_name(seq=None):
         if seq is None:
-            return os.path.join(output_dir, anchor_name + ".md")
+            return anchor_name
         else:
-            return os.path.join(output_dir, "%s_%i.md" % (anchor_name, seq))
+            return "%s_%i" % (anchor_name, seq)
 
-    default_name = "front_matter"
+    def full_path(seq = None):
+        return os.path.join(output_dir, seq_name(seq) + ".md")
+
+    default_name = "index"
     if anchor_name is None:
         anchor_name = default_name
 
-    if not os.path.exists(assemble_name()):
-        return assemble_name()
+    if not os.path.exists(full_path()):
+        return seq_name()
 
     # If the file already exists, try "foo_1.md", "foo_2,md", ...
     n = 1
-    while os.path.exists(assemble_name(n)):
+    while os.path.exists(full_path(n)):
         n += 1
-    return assemble_name(n)
+    return seq_name(n)
 
 
 def print_to_markdown(processed_section, output_dir, relpath):
@@ -53,7 +57,12 @@ def print_to_markdown(processed_section, output_dir, relpath):
     # "listfilepages\globalfilestagpages\globalfilesbonus.html"
 
     anchor = processed_section["anchor"]
-    output_file_path = generate_file_name(output_dir, anchor)
+    output_file_name = generate_file_name(output_dir, anchor)
+    output_file_path = os.path.join(output_dir, output_file_name + ".md")
+    if processed_section["anchor"] is None and processed_section["md"].strip() == "":
+        assert "index" in output_file_path
+        print ("Skipping: %s (blank)" % output_file_path)
+        return
     print ("Writing: %s" % output_file_path)
 
     date = str(datetime.date.today())
@@ -70,8 +79,12 @@ def print_to_markdown(processed_section, output_dir, relpath):
     dir, file = os.path.split(relpath)
     parent = file.replace(".html", "")
 
+    id = parent + "_" + output_file_name
+    # if anchor:
+    #     id = id + "_" + anchor
+
     content = md_template.format(original_url=original_url, date=date,
-                                 parent=parent, **processed_section)
+                                 parent=parent, id = id, **processed_section)
 
     with open(output_file_path, "wb") as out_file:
         out_file.write(content.encode('utf-8'))
