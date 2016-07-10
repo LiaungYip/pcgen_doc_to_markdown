9db9d06dca9e395c7ea4ead9784851558648000d
Li-aung 'Lewis' Yip <liaung@gmail.com>
2016-07-10 19:49:26 +0800

    Helper script to generate menu section of Hugo's config.toml

---
+++ make_menu.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+
+from file_list import files
+
+files = [f.replace(".html", "") for f in files]
+files = [f.replace("\\", "/") for f in files]
+
+# prefixes = set()
+# for f in files:
+#     a, b = os.path.split(f)
+#     prefixes.add((a,b,))
+#     while a:
+#         a, b = os.path.split(a)
+#         prefixes.add((a,b,))
+
+pairs = set()
+
+for f in files:
+    parts = f.split("/")
+    for parent, child in zip(parts[:-1], parts[1:]):
+        pairs.add((parent, child))
+
+pprint(pairs)
+
+template = """[[menu.main]]
+    name = "{name}"
+    identifier = "{child}"
+    parent = "{parent}"
+"""
+for pair in pairs:
+    parent = pair[0]
+    child = pair[1]
+
+    name = child
+    for trash in ["filestagpages","datafiles","globalfiles","gamemode","systemfiles"]:
+        name = name.replace(trash,"")
+
+    print template.format(parent = parent, child = child, name = name)
